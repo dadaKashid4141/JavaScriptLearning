e65d81f019fcb740e472ec0231c4ee68142a2658
Rahul <rahulingole2112@gmail.com>
2022-10-04 19:34:16 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -4,5 +4,6 @@ console.info("Learning,hubble,Hard working");
 console.info("doing one work at one time,trust  on everyone,emotional");
 console.log(30+50);
 console.log(49*479);
+.console.log("hi dada");
 
 
