a5561c4faba547efc943ac8068d0760953c9eae9
Rahul <rahulingole2112@gmail.com>
2022-10-04 19:29:20 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,6 +3,7 @@ console.log("Watching Movies,Photography,traveling");
 console.info("Learning,hubble,Hard working");
 console.info("doing one work at one time,trust  on everyone,emotional");
 console.log("30+50");
+console.log("Hi I am Dada");
 console.log(49*479);
 
 
