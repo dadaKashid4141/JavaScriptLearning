67dea86a89156ec1e83971e14c9c88803454c514
Rahul <rahulingole2112@gmail.com>
2022-10-04 19:24:18 +0530

    demo.js
    demo

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ console.log("Dada Ashok Kashid");
 console.log("Watching Movies,Photography,traveling");
 console.info("Learning,hubble,Hard working");
 console.info("doing one work at one time,trust  on everyone,emotional");
-console.log(30+50);
+console.log("1+2")
 console.log(49*479);
 
 
