63bf0547e008eb1ab1211487acd7949396d57d76
dadaKashid4141 <dadakashid4141@gmail.com>
2022-10-04 19:53:49 +0530

    o
    l

--- demo.js
+++ demo.js
@@ -3,15 +3,4 @@ console.log("Watching Movies,Photography,traveling");
 console.info("Learning,hubble,Hard working");
 console.info("doing one work at one time,trust  on everyone,emotional");
 console.log(30+50);
-<<<<<<< Updated upstream
 console.log(49*479);
-<<<<<<< Updated upstream
-.console.log("hi dada");
-=======
-console.log(50*479);
->>>>>>> Stashed changes
-=======
-console.log("hi dada");
->>>>>>> Stashed changes
-
-
