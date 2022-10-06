1f09aab36c757a6426842a642b0a5ed3428d0360
dadaKashid4141 <dadakashid4141@gmail.com>
2022-10-06 19:53:22 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,7 +3,5 @@ console.log("Watching Movies,Photography,traveling");
 console.info("Learning,hubble,Hard working");
 console.info("doing one work at one time,trust  on everyone,emotional");
 console.log(30+50);
+
 console.log(49*479);
-
-
-
